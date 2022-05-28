af2272c1b84e8efb993e843c85f1e8dd66e99b0a
Adrian Liermann <liermann.adrian@gmail.com>
2022-05-28 14:28:13 +0200

    Adds cli for specific tasks of the etl process

--- src/logfile_etl/logfile_etl.py
+++ src/logfile_etl/logfile_etl.py
@@ -7,14 +7,49 @@ from src.logfile_etl.logfile_merger import LogMerger
 from src.logfile_etl.merged_logfile_processor import MergedLogProcessor
 from src.logfile_etl.configuration_handler import ConfigurationHandler
 
-logging.basicConfig(format="%(levelname)s:%(message)s", level=logging.INFO)
+logging.basicConfig(
+    format="%(asctime)s %(levelname)s %(message)s",
+    level=logging.INFO
+)
+
+app = typer.Typer()
 
 
-def main(config_file_path: str = "resources/config/etl_config.json"):
+@app.command()
+def convert_logs(config_file_path: str = "resources/config/etl_config.json"):
     config_handler = ConfigurationHandler(config_file_path)
     config_handler.load_config()
 
-    # TODO cli for single action use cases
+    log_converter = LogfileConverter(config_handler)
+    log_converter.convert_logfiles()
+
+
+@app.command()
+def merge_logs(config_file_path: str = "resources/config/etl_config.json"):
+    config_handler = ConfigurationHandler(config_file_path)
+    config_handler.load_config()
+
+    log_merger = LogMerger(config_handler)
+    log_merger.merge_logfiles()
+
+
+@app.command()
+def extract_features(
+        config_file_path: str = "resources/config/etl_config.json"
+):
+    config_handler = ConfigurationHandler(config_file_path)
+    config_handler.load_config()
+
+    feature_extractor = MergedLogProcessor(config_handler)
+    feature_extractor.process_merged_logs()
+    feature_extractor.save_features_to_db()
+
+
+@app.callback(invoke_without_command=True)
+def run_etl_process(config_file_path: str = "resources/config/etl_config.json"):
+    config_handler = ConfigurationHandler(config_file_path)
+    config_handler.load_config()
+
     log_converter = LogfileConverter(config_handler)
     log_converter.convert_logfiles()
     log_merger = LogMerger(config_handler)
@@ -26,4 +61,4 @@ def main(config_file_path: str = "resources/config/etl_config.json"):
 
 
 if __name__ == "__main__":
-    typer.run(main)
+    app()
