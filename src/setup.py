eae10b619bb4bb39ce914c88f8f534e66847dafc
Adrian Liermann <liermann.adrian@gmail.com>
2022-05-30 18:01:08 +0200

    fix setup script

--- src/setup.py
+++ src/setup.py
@@ -1,6 +1,7 @@
 import json
 import pathlib
 from datetime import datetime
+import os
 
 if __name__ == "__main__":
     parent_dir = pathlib.Path().absolute().parent
@@ -41,12 +42,43 @@ if __name__ == "__main__":
                    "AdaLR", "AdaDT", "DT"],
         "model_save_path": model_folder.as_posix()
     }
+    configuration_folder = resource_folder / "config"
 
-    analysis_config_file_path = resource_folder / "config" / "analysis_config.json"
-    etl_config_file_path = resource_folder / "config" / "etl_config.json"
+    analysis_config_file_path = configuration_folder / "analysis_config.json"
+    etl_config_file_path = configuration_folder / "etl_config.json"
 
-    with open(analysis_config_file_path, 'w') as fp:
+    os.makedirs(configuration_folder.as_posix(), exist_ok=True)
+
+
+    with open(analysis_config_file_path, 'x') as fp:
         json.dump(analysis_config, fp)
 
-    with open(etl_config_file_path, 'w') as fp:
+    with open(etl_config_file_path, 'x') as fp:
         json.dump(etl_config, fp)
+
+    print("Configuration files created in: {}".format(configuration_folder))
+
+    os.makedirs(unprocessed_folder.as_posix(), exist_ok=True)
+    print(
+        "Folder for unprocessed logfiles is created at: {}".format(
+            unprocessed_folder.as_posix()
+        )
+    )
+    os.makedirs(processed_folder.as_posix(), exist_ok=True)
+    print(
+        "Folder for processed logfiles is created at: {}".format(
+            processed_folder.as_posix()
+        )
+    )
+    os.makedirs(db_folder.as_posix(), exist_ok=True)
+    print("Folder for db export is created at: {}".format(db_folder.as_posix()))
+    os.makedirs(csv_folder.as_posix(), exist_ok=True)
+    print(
+        "Folder for csv export is created at: {}".format(csv_folder.as_posix())
+    )
+    os.makedirs(model_folder.as_posix(), exist_ok=True)
+    print(
+        "Folder for model export is created at: {}".format(
+            model_folder.as_posix()
+        )
+    )
