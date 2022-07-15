8b863702a8b0ee950f8b525ad2b0e62f8b91362b
Adrian Liermann <liermann.adrian@gmail.com>
2022-07-15 17:48:25 +0200

    add all changes

--- src/regression_analysis/features/list_parallel_command_finished_extractor.py
+++ src/regression_analysis/features/list_parallel_command_finished_extractor.py
@@ -1,7 +1,11 @@
 import json
+from abc import ABC
+from ast import literal_eval
 
 import pandas as pd
-from numpy import short
+from numpy import uint8
+from sqlalchemy import select, types, MetaData, Table, Column, Integer, String
+import numpy as np
 
 from src.regression_analysis.configuration_handler import ConfigurationHandler
 from src.regression_analysis.database import Database
@@ -10,43 +14,63 @@ from src.regression_analysis.features.abstract_feature_extractor import (
 )
 
 
+class JSONEncodedDict(types.TypeDecorator):
+    impl = types.TEXT
+
+    cache_ok = True
+
+    def process_bind_param(self, value, dialect):
+        if value is not None:
+            value = json.dumps(value)
+        return value
+
+    def process_result_value(self, value, dialect):
+        return literal_eval(value)
+
+
 class ListParallelRequestsFinished(AbstractFeatureExtractor):
     def get_column_name(self) -> str:
         return "List parallel requests finished"
 
     def get_df(self, db, names_mapping) -> pd.DataFrame:
-        df = self.load_df_column_from_db(db)
-        print(type(df.iloc[0][0]))
-        # print(df.dtypes)
-        # print(df)
-        # print("---------")
-        # print(df)
-        # print("---------")
-        # print("map to columns")
-       #print(df[self.get_column_name()].values.tolist())
-        #df = pd.DataFrame(df.pop(self.get_column_name()).values.tolist())
-        #print(df)
-        # print("add prefix")
-        # df = df.add_prefix("last_finished_")
-        # print(df)
-        # print("---------")
-        # print("fillna")
-        # df = df.fillna(0)
-        # print(df)
-        #return df.astype(short, copy=False)
+        metadata_obj = MetaData()
+        data = Table(
+            'gs_training_data', metadata_obj,
+            Column('index', Integer, primary_key=True),
+            Column('List_parallel_requests_finished', JSONEncodedDict),
+        )
+        names_mapping = Table(
+            'gs_training_cmd_mapping', metadata_obj,
+            Column('index', Integer, primary_key=True),
+            Column('mapping', String),
+        )
 
+        con = db.connection()
 
-    def __apply_func(self, row, names_mapping):
-        command_dict = json.loads(row)
-        numerical_dict = {}
-        for key, value in command_dict.items():
-            numerical_dict["end_list_cmd_{}".format(
-                self.get_cmd_int(key, names_mapping)
-            )] = value
-        return numerical_dict
+        mapping_query = select(names_mapping)
+        mapping_result = con.execute(mapping_query).scalars().all()
+
+        data_query = select(data)
+        data_result = con.execute(data_query).all()
+
+        array = np.zeros(
+            shape=(len(data_result), len(mapping_result) + 1),
+            dtype=uint8
+        )
+
+        for index, col in data_result:
+            if len(col) > 0:
+                for key, val in col.items():
+                    array[int(index), int(key)] = val
+
+        df = pd.DataFrame(array, dtype=uint8)
+        return df
+
 
 if __name__ == "__main__":
-    config_handler = ConfigurationHandler("resources/config/analysis_config.json")
+    config_handler = ConfigurationHandler(
+        "resources/config/analysis_config.json"
+    )
     config_handler.load_config()
     database = Database(config_handler)
     test = ListParallelRequestsFinished()
--- src/regression_analysis/features/list_parallel_command_start_extractor.py
+++ src/regression_analysis/features/list_parallel_command_start_extractor.py
@@ -1,11 +1,29 @@
 import json
+from abc import ABC
+from ast import literal_eval
 
 import pandas as pd
-from numpy import short
+from numpy import uint8
+from sqlalchemy import select, types, MetaData, Table, Column, Integer, String
+import numpy as np
 
 from src.regression_analysis.features.abstract_feature_extractor import (
     AbstractFeatureExtractor,
 )
+
+
+class JSONEncodedDict(types.TypeDecorator):
+    impl = types.TEXT
+
+    cache_ok = True
+
+    def process_bind_param(self, value, dialect):
+        if value is not None:
+            value = json.dumps(value)
+        return value
+
+    def process_result_value(self, value, dialect):
+        return literal_eval(value)
 
 
 class ListParallelRequestsStart(AbstractFeatureExtractor):
@@ -13,18 +31,36 @@ class ListParallelRequestsStart(AbstractFeatureExtractor):
         return "List parallel requests start"
 
     def get_df(self, db, names_mapping) -> pd.DataFrame:
-        df = self.load_df_column_from_db(db)
-        df.add_prefix("list_start_")
-        df.join(pd.DataFrame(df.pop(self.get_column_name()).values.tolist()))
-        df.fillna(0)
-        print(df)
-        return df.astype(short, copy=False)
 
-    def __apply_func(self, row, names_mapping):
-        command_dict = json.loads(row)
-        numerical_dict = {}
-        for key, value in command_dict.items():
-            numerical_dict["start_list_cmd_{}".format(
-                self.get_cmd_int(key, names_mapping)
-            )] = value
-        return numerical_dict
+        metadata_obj = MetaData()
+        data = Table(
+            'gs_training_data', metadata_obj,
+            Column('index', Integer, primary_key=True),
+            Column('List_parallel_requests_start', JSONEncodedDict),
+        )
+        names_mapping = Table(
+            'gs_training_cmd_mapping', metadata_obj,
+            Column('index', Integer, primary_key=True),
+            Column('mapping', String),
+        )
+
+        con = db.connection()
+
+        mapping_query = select(names_mapping)
+        mapping_result = con.execute(mapping_query).scalars().all()
+
+        data_query = select(data)
+        data_result = con.execute(data_query).all()
+
+        array = np.zeros(
+            shape=(len(data_result), len(mapping_result) + 1),
+            dtype=uint8
+        )
+
+        for index, col in data_result:
+            if len(col) > 0:
+                for key, val in col.items():
+                    array[int(index), int(key)] = val
+
+        df = pd.DataFrame(array, dtype=uint8)
+        return df
