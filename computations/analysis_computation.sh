4b11bbf2b19f28f46e3815d9e9c2e84270fc066c
Adrian Liermann <liermann.adrian@gmail.com>
2022-07-30 17:38:32 +0200

    changed analysis_computation for normal node

--- computations/analysis_computation.sh
+++ computations/analysis_computation.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-#SBATCH --nodes=2               # number of nodes to reserve
-#SBATCH --ntasks-per-node=6     # number of CPU cores per node
-#SBATCH --mem=64G		        # RAM memory to reserve
-#SBATCH --partition=express     # partition to submit job to # test node: express
-#SBATCH --time=2:00:00         # max wallclock time (i.e. job exec time limit)
+#SBATCH --nodes=1               # number of nodes to reserve
+#SBATCH --ntasks-per-node=12    # number of CPU cores per node
+#SBATCH --mem=128G		        # RAM memory to reserve
+#SBATCH --partition=normal     # partition to submit job to # test node: express
+#SBATCH --time=24:00:00          # max wallclock time (i.e. job exec time limit)
 
 #SBATCH --job-name=create_model    # only ~30 characters shown when using squeue 
-#SBATCH --output=logfile_analysis.dat   # important for debugging etc!! name individually
+#SBATCH --output=logfile_analysis_normal.dat   # important for debugging etc!! name individually
 #SBATCH --mail-type=ALL         # receive email for start | finish | abortion
 #SBATCH --mail-user=a_lier03@uni-muenster.de
 
