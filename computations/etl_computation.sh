638048763bf56c98d56378cf4a7d5d448fc8b0c8
Adrian Liermann <liermann.adrian@gmail.com>
2022-09-17 10:07:36 +0200

    add computation files

--- computations/etl_computation.sh
+++ computations/etl_computation.sh
@@ -2,12 +2,12 @@
 
 #SBATCH --nodes=1               # number of nodes to reserve
 #SBATCH --ntasks-per-node=6     # number of CPU cores per node
-#SBATCH --mem=16G		        # RAM memory to reserve
+#SBATCH --mem=50G		        # RAM memory to reserve
 #SBATCH --partition=express     # partition to submit job to # test node: express
 #SBATCH --time=2:00:00         # max wallclock time (i.e. job exec time limit)
 
 #SBATCH --job-name=extract_logfile    # only ~30 characters shown when using squeue 
-#SBATCH --output=logfile_extraction.dat   # important for debugging etc!! name individually
+#SBATCH --output=logfile_extraction-%J.dat   # important for debugging etc!! name individually
 #SBATCH --mail-type=ALL         # receive email for start | finish | abortion
 #SBATCH --mail-user=a_lier03@uni-muenster.de
 
@@ -21,6 +21,11 @@ module load Python/3.9.5
 module load scikit-learn/0.24
 module laod SQLite/3.35.4
 
+echo "------------------------------------------------------------"
+echo "SLURM JOB ID: $SLURM_JOBID"
+echo "Running on nodes: $SLURM_NODELIST"
+echo "------------------------------------------------------------"
+
 
 echo    # functions like a print()-call; here, it creates a blank line, because no text is provided
 
@@ -28,7 +33,7 @@ echo    # functions like a print()-call; here, it creates a blank line, because 
 
 # explicit job to run, e.g. python
 
-python ../src/logfile_etl/logfile_etl.py run
+python ../src/logfile_etl/logfile_etl.py extract-features
 
 
 
--- computations/test.sh
+++ computations/test.sh
@@ -2,14 +2,14 @@
 
 
 #SBATCH --nodes=1
-#SBATCH --cpus-per-task=8
-#SBATCH --mem=150gb		        # RAM memory to reserve
-#SBATCH --partition=normal     # partition to submit job to # test node: express
-#SBATCH --time=24:00:00          # max wallclock time (i.e. job exec time limit)
+#SBATCH --cpus-per-task=2
+#SBATCH --mem=20gb		        # RAM memory to reserve
+#SBATCH --partition=express     # partition to submit job to # test node: express
+#SBATCH --time=1:00:00          # max wallclock time (i.e. job exec time limit)
 
 
-#SBATCH --job-name=grid_search_ridge    # only ~30 characters shown when using squeue
-#SBATCH --output=grid_search_cmd_one_hot_prs_ridge_%J.log   # important for debugging etc!! name individually
+#SBATCH --job-name=test    # only ~30 characters shown when using squeue
+#SBATCH --output=test_%J.log   # important for debugging etc!! name individually
 #SBATCH --mail-user=a_lier03@uni-muenster.de
 
 # Instead of Module Loading here, load them prior to execution and run script as "bash -i <script_name>.sh"
@@ -20,10 +20,6 @@ module load OpenMPI/4.1.1
 module load Python/3.9.5
 module load scikit-learn/0.24
 # module laod SQLite/3.35.4
-
-# export OMP_PROC_BIND=TRUE
-# export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
-# export NUMEXPR_MAX_THREADS=$SLURM_CPUS_PER_TASK
 
 
 echo    # functions like a print()-call; here, it creates a blank line, because no text is provided
@@ -39,7 +35,7 @@ echo "------------------------------------------------------------"
 # explicit job to run, e.g. python
 
 #python ../src/test.py
-python ../src/analysis/regression_analysis.py
+python ../resources/export/db/test.py
 
 
 
